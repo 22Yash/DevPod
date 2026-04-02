4dac326b572c874b295a849e0587a7566d8b0b90
Bharat Choudhary <bharatchoudhary1602@gmail.com>
2026-04-02 15:02:21 +0530

    fix: mern localhost final

--- docker/mern-template/frontend/vite.config.js
+++ docker/mern-template/frontend/vite.config.js
@@ -4,14 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/absproxy/3000/',
+  base: './',
   server: {
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
-    hmr: {
-      path: '/absproxy/3000/',
-    },
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
